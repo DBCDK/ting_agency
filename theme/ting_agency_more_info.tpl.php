cd035ec16af6244b8e54f35d7d9c6b349042ec73
pjohans <pjohans@gmail.com>
2012-11-13 11:05:26 +0100

    theme in ting_agency

---
+++ theme/ting_agency_more_info.tpl.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+dpm($variables);
+?>
+
+
+<div class="element-section visuallyhidden">
+  <div class="library-details text clearfix">
+    <div class="library-details-column-1">
+      <?php //if( isset($address) ) : ?>
+      <p class="subheader"><strong><?php print t('bibdk_favourite_address'); ?></strong></p>
+      <p><?php print $address; ?></p>
+      <p class="subheader"><strong><?php print t('bibdk_favourite_contact'); ?></strong></p>
+      <p><?php print $contact; ?></p>
+      <?php //endif; ?>
+    </div> <!-- column-1 -->
+
+    <div class="library-details-column-2">
+      <?php if ( $openingHours != 'ting_agency_no_opening_hours' ) { ?>
+        <p class="subheader"><strong><?php print t('bibdk_favourite_opening_hours'); ?></strong></p>
+        <p><?php print $openingHours; ?></p>
+      <?php } ?>
+      <?php if ( $tools ) { ?>
+        <p class="subheader"><strong><?php print t('bibdk_favourite_tools'); ?></strong></p>
+        <?php print $tools; ?>
+      <?php } ?>
+    </div>
+  </div>
+</div>
