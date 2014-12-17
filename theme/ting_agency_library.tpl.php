e7b6ab144d50ff1a93aa491b52d38f8825d9e912
Martin Møller <mmj@dbc.dk>
2014-12-17 15:52:38 +0100

    reversing commit as it was commited to wrong branch

--- theme/ting_agency_library.tpl.php
+++ theme/ting_agency_library.tpl.php
@@ -4,36 +4,37 @@
  * and open the template in the editor.
  */
 ?>
-<article class="<?php print $classes ?>">
-  <div class="element-section clearfix">
-    <div class="hgroup<?php print !empty($actions) ? ' has-action' : ''; ?>">
-      <span class="ting-agency-branch-name"><?php print $branchName; ?></span>
-
-      <?php if (!empty($agencyName)) : ?>
-        <span class="ting-agency-agency-name"><?php print $agencyName; ?></span>
-        <?php if (!empty($temporarilyClosedReason)) : ?>
-          <strong><br/><?php print $temporarilyClosedReason; ?></strong>
+    <article class="<?php print $classes ?>">
+      <div class="element-section clearfix">
+        <div class="hgroup<?php print !empty($actions) ? ' has-action' : ''; ?>">
+          <h3><?php print $branchName; ?></h3>
+          <?php if (isset($agencyName)) : ?>
+            <h4><?php print $agencyName; ?></h4>
+            <?php if (isset($temporarilyClosedReason)) : ?>
+              <strong><br/><?php print $temporarilyClosedReason; ?></strong>
+            <?php endif; ?>
+          <?php endif; ?>
+        </div>
+        <?php if (!empty($actions)) : ?>
+          <div class="actions">
+            <?php print $actions; ?>
+          </div>
         <?php endif; ?>
+        <div class="messages_<?php print $branchid ?> clearfix"></div>
+        <?php if (isset($moreinfo) and $toggle_enabled == true) : ?>
+        <div class="toggle-next-section">
+          <a href="#" class="show-more">
+            <strong><?php print t('ting_agency_more_info'); ?></strong>
+          </a>
+          <a href="#" class="show-less visuallyhidden">
+            <strong><?php print t('ting_agency_less_info'); ?></strong>
+          </a>
+        </div>
+      </div>
       <?php endif; ?>
-    </div>
-    <?php if (!empty($actions)) : ?>
-      <?php print $actions; ?>
-    <?php endif; ?>
-    <div class="messages_<?php print $branchid ?> clearfix"></div>
-    <?php if (isset($moreinfo) and $toggle_enabled == true) : ?>
-    <div class="toggle-next-section">
-      <a href="#" class="show-more">
-        <strong><?php print t('ting_agency_more_info'); ?></strong>
-      </a>
-      <a href="#" class="show-less visuallyhidden">
-        <strong><?php print t('ting_agency_less_info'); ?></strong>
-      </a>
-    </div>
-  </div>
-  <?php endif; ?>
-  <?php if (!empty($moreinfo)): ?>
-    <?php print $moreinfo; ?>
-  <?php endif; ?>
-</article>
+      <?php if (isset($moreinfo)): ?>
+        <?php print $moreinfo; ?>
+      <?php endif; ?>
+    </article>
 
 
