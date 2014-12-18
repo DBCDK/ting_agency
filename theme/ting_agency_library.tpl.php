ae824e953d5655c8980cfea38df5f9f9c4b36853
Martin Møller <mmj@dbc.dk>
2014-12-18 13:48:25 +0100

    - removed more/less info

--- theme/ting_agency_library.tpl.php
+++ theme/ting_agency_library.tpl.php
@@ -4,34 +4,25 @@
  * and open the template in the editor.
  */
 ?>
-<article class="<?php print $classes ?>">
+<article class="<?php print $classes; ?>" data-ting-agency-more-info-toggler>
   <div class="element-section clearfix">
-    <div class="hgroup<?php print !empty($actions) ? ' has-action' : ''; ?>">
-      <span class="ting-agency-branch-name"><?php print $branchName; ?></span>
-
+    <div class="ting-agency--header">
+      <span class="ting-agency--branch-name"><?php print $branchName; ?></span>
       <?php if (!empty($agencyName)) : ?>
-        <span class="ting-agency-agency-name"><?php print $agencyName; ?></span>
+        <span class="ting-agency--agency-name"><?php print $agencyName; ?></span>
         <?php if (!empty($temporarilyClosedReason)) : ?>
           <strong><br/><?php print $temporarilyClosedReason; ?></strong>
         <?php endif; ?>
       <?php endif; ?>
     </div>
     <?php if (!empty($actions)) : ?>
-      <?php print $actions; ?>
+      <div class="ting-agency--actions"><?php print $actions; ?></div>
     <?php endif; ?>
     <div class="messages_<?php print $branchid ?> clearfix"></div>
-    <?php if (isset($moreinfo) and $toggle_enabled == true) : ?>
-    <div class="toggle-next-section">
-      <a href="#" class="show-more">
-        <strong><?php print t('ting_agency_more_info'); ?></strong>
-      </a>
-      <a href="#" class="show-less visuallyhidden">
-        <strong><?php print t('ting_agency_less_info'); ?></strong>
-      </a>
-    </div>
   </div>
-  <?php endif; ?>
   <?php if (!empty($moreinfo)): ?>
-    <?php print $moreinfo; ?>
+    <div class="element-section ting-agency--more-info">
+      <?php print $moreinfo; ?>
+    </div>
   <?php endif; ?>
 </article>
